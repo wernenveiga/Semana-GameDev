d4f6e4ff2a5782bc4e9fa286440b3648115c5c55
Wernen Veiga <wernen_veiga@outlook.com>
2020-07-01 17:55:42 -0400

    2 lesson
    Development of the first actions of the game

---
+++ Assets/BowScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BowScript : MonoBehaviour
+{
+    public Transform Player;
+    public float offset = 1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var dir = Input.mousePosition - Camera.main.WorldToScreenPoint(transform.position);
+        var angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+
+        Vector3 playerToMouseDir = Camera.main.ScreenToWorldPoint(Input.mousePosition) - Player.position;
+        playerToMouseDir.z = 0;
+        transform.position = Player.position + (offset * playerToMouseDir.normalized);
+    }
+}
---
+++ Assets/PlayerScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+
+    private Vector2 direction;
+    
+    public float moveSpeed;
+    public Rigidbody2D playerRb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        direction.x = Input.GetAxisRaw("Horizontal");
+        direction.y = Input.GetAxisRaw("Vertical");
+    }
+
+    private void FixedUpdate(){
+        playerRb.MovePosition(playerRb.position + (direction * moveSpeed * Time.fixedDeltaTime));
+    }
+
+}
